7f7c266a722ee37058f7311f87f3fd17f8709507
kxznn <kauaroaguiar16@gmail.com>
2026-08-18 20:16:32 -0300

    Aula 03 - Django Project

--- aula03/produtos/admin.py
+++ aula03/produtos/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+@admin.register (Produto)
+
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ("id", "nome", "quantidade", "preco", "created_at")
+    search_fields = ("nome",)
+    
--- aula03/produtos/apps.py
+++ aula03/produtos/apps.py
@@ -2,4 +2,5 @@ from django.apps import AppConfig
 
 
 class ProdutosConfig(AppConfig):
+    default_auto_filed = 'django.db.models.BigAutoFiled'
     name = 'produtos'
