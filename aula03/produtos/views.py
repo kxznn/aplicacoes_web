4e682861c4caadf4b896a284c1e6b1dddb0cf8a0
kxznn <kauaroaguiar16@gmail.com>
2026-08-18 20:37:01 -0300

    ViewSet Crude

--- aula03/produtos/views.py
+++ aula03/produtos/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework import viewstes # importa o viewset a partir da biblioteca restframwwork
+from .models import Produto
+from .serializers import ProdutosSerializer
 
+# importando metodo para exibir uma pagina home
+
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Olá Django ! Aplicações Web 2026 - 2 - Aula  02 Loja de Produtos")
+
+    # Cria a classe Produtosviewset responsável por permitir fazer o crude
+
+class ProdutoViewSet(viewstes.ModelViewset):
+    queryset + Produto.objects.all().order_by("-id")
+    serializer_class = ProdutosSerializer
+
+    
 # Create your views here.
