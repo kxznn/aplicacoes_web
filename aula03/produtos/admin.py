c2e781c7ab275dcd1d4e513fa99219020c177f03
kxznn <kauaroaguiar16@gmail.com>
2026-08-20 20:30:47 -0300

    modified admin

--- aula03/produtos/admin.py
+++ aula03/produtos/admin.py
@@ -1,10 +1,32 @@
 from django.contrib import admin
-from .models import Produto  # importa a partir do models o produto
-
+from .models import (
+    Produto,  # importa a partir do models o produto
+    Categoria,
+    Cliente,
+    Pedido,
+    ItemPedido
+    )
 # Register your models here
 
+
+# Registrando a categoria no admin
+
+@admin.register(Categoria)
+
+class CategoriaAdmin(admin.Model.Admin):
+
+    list_display = ('id', 'nome')
+
+    # filtro searchfield
+
+    search_fileds = ('nome',)
 @admin.register(Produto)
 
 class ProdutoAdmin(admin.ModelAdmin):
-    list_display = ("id", "nome", "quantidade", "preco", "created_at")
+    list_display = ("id", "nome", "quantidade", "preco", "categoria", "created_at")
     search_fields = ("nome",)
+    list_filter = ("categoria")
+
+
+# cliente 
+
